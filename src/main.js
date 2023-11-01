f4164b8870aaeaf32eb44bbc03b2097bd77550e1
Necho <2839868773@qq.com>
2023-11-02 01:43:42 +0800

    界面框架确定

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
 import App from './App.vue'
+// 注册Ant全局组件
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+// 注册状态管理组件
+import {createPinia} from 'pinia'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(Antd)
+app.use(pinia)
+app.mount('#app')
---
+++ src/views/Layout.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="root">
+		<div class="navigation">
+			<div class="navigation-content"/>
+		</div>
+		<div class="header">
+			<div class="header-content"/>
+		</div>
+		<div class="content">
+			<div class="main-content"/>
+		</div>
+	</div>
+</template>
+<script>
+
+</script>
+<style scoped>
+.root {
+	width: 100vw;
+	height: 100vh;
+	background: no-repeat url("@/assets/background.png") rgba(22, 24, 35);
+	position: fixed;
+}
+
+.header {
+	position: fixed;
+	right: 0;
+	height: 65px;
+	width: calc(100% - 160px);
+	background-color: rgba(22, 24, 35, .3);
+	border-bottom: 1px solid rgba(99, 99, 101, 0.3);
+	box-shadow: 0 2px 5px #00000014;
+	z-index: 999;
+}
+
+.navigation {
+	position: fixed;
+	width: 160px;
+	height: 100%;
+	background-color: rgba(22, 24, 35, .3);
+	border-right: 1px solid rgba(99, 99, 101, 0.3);
+	z-index: 999;
+}
+
+.content {
+	position: absolute;
+	top: 65px;
+	left: 160px;
+	width: calc(100% - 170px);
+	height: calc(100% - 71px);
+	border-radius: 15px;
+	margin: 3px 5px;
+	padding: 20px 25px;
+	overflow: hidden;
+	transition: all .15s linear;
+}
+</style>
